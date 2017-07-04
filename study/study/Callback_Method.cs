583b15f5b0131add9ef8fd9965cca35cb2a386a8
XxshiftxX <qjfrntop12@naver.com>
2017-07-04 15:26:11 +0900

    C# 공부

---
+++ study/study/Callback_Method.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+delegate int GetResultDelegate();
+
+class Target
+{
+	public void Do(GetResultDelegate getResult)
+    {
+        Console.WriteLine(getResult());
+    }
+}
+
+class Source
+{
+    public int GetResult()
+    {
+        return 10;
+    }
+
+    public void Test()
+    {
+        Target target = new Target();
+        target.Do(new GetResultDelegate(this.GetResult));
+    }
+
+}
+
+public class main
+{
+    public static void Main()
+    {
+        new Source().Test();
+    }
+}
---
+++ study/study/Interface.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace study
+{
+    class Computer
+    {
+    }
+
+    interface IMonitor
+    {
+        void TurnOn();
+    }
+
+    interface IKeyboard { }
+
+    class Notebook : Computer, IMonitor, IKeyboard
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("TurnOn!");
+        }
+    }
+
+    class Desktop : Computer, IMonitor, IKeyboard
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Desktop Computer Booted!");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Notebook n = new Notebook();
+            n.TurnOn();
+            IMonitor i = n;
+            i.TurnOn();
+            IMonitor i2 = new Desktop();
+            i2.TurnOn();
+        }
+    }
+}
